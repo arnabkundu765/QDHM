61974cd872d6d71ab20590ec385a764920a8b969
Arnab Kundu <arnab.kundu1@in.ey.com>
2026-09-01 17:47:18 +0530

    initial

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QDHM.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
